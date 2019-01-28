c2f476822062bc376b6e4aabb0a8c1003d49f486
Ben Abroms <babroms@una.edu>
2019-01-28 16:38:25 +0000

    Daily work 1-28-19

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200);    // HTML 200 is successful connection code
+                                // I.E. similar to Error 404
+    response.write("<html><body><p>Hello, this is doge.</p></body></html>");
+    setTimeout(function(){
+        response.write("<html><body><p>Doge is tired now.</p></body></html>");
+        response.end();
+    }, 2000);
+    response.end();
+}).listen(8080);
+
+console.log('Listening on port 8080...');
